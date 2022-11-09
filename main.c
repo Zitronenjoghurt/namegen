0fc3bda57fa27a51b18c9a76c8d755cf276d96e7
Zitronenjoghurt <gersdorfdominik@gmail.com>
2022-11-09 19:24:01 +0100

    add string constants and i/o

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
 
+const char consonants[] = {'a', 'e', 'i', 'o', 'u'};
+const char vocals[] = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
+
 int main()
 {
-	printf("Käse");
+	unsigned int length = 1;
+
+	printf("How long should the name be?\n");
+
+	fflush(stdout);
+
+	if (scanf_s("%i", &length) == 1) {
+		printf("You want a name thats %i characters long.\n", length);
+	}
+
 	return 0;
 }
